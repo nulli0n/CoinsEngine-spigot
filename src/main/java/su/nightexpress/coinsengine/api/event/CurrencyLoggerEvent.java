7aee97e064412d0718c1bd0f52401868fecc4ac1
BestZige <bestzige@hotmail.com>
2025-07-03 17:28:36 +0700

    chore: add CurrencyLoggerEvent
    Purpose of Log Centering in Data Analysis

---
+++ src/main/java/su/nightexpress/coinsengine/api/event/CurrencyLoggerEvent.java
@@ -0,0 +1,52 @@
+package su.nightexpress.coinsengine.api.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author BestZige
+ * Created: 7/3/2025
+ */
+
+public class CurrencyLoggerEvent extends Event implements Cancellable {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private final String message;
+
+    private boolean cancelled;
+
+    public CurrencyLoggerEvent(@NotNull String message) {
+        super(!Bukkit.isPrimaryThread());
+        this.message = message;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @NotNull
+    public String getMessage() {
+        return this.message;
+    }
+}
+
